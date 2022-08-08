bb05b8602fed1a0c38ccad5fd77b101d8b58eb1f
EC2 Default User <ec2-user@ip-172-31-75-144.ec2.internal>
2022-08-08 16:31:31 +0000

    課題2 サクセスメッセージ作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(book.id)
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
+      @user = User.find(current_user.id)
       render :index
     end
   end
@@ -17,11 +19,13 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = User.find(current_user.id)
   end
 
   def show
     @book = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def edit
@@ -44,6 +48,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 end
