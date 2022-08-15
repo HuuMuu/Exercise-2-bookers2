6b33322510b5c6cf9268ef2f5f273a08ad5a4a2a
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-08-15 09:11:35 +0000

    課題2エラー直し中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "57aaa242d3b5463bbcc90c34dc0bfe8f.vfs.cloud9.us-east-1.amazonaws.com"
   config.active_job.queue_adapter = :inline
+  config.hosts.clear
+
 end
