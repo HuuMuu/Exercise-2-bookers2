72b069f1b9c37686c8c044281c8ffaa49d438228
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-08-18 12:09:00 +0000

    課題2 再提出用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def correct_user
--- db/migrate/20220801163430_adduser.rb
+++
@@ -1,4 +0,0 @@
-class Adduser < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
